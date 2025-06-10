5375865d4e9528e3507e4b7da07187ddf73eca4c
Jeet Mukherjee <jeetmukherjee100@gmail.com>
2025-06-10 12:01:01 +0530

    update theme changing

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,105 @@
-import {Button} from '@/components/ui/button';
-import {
-  FileTextIcon, Sun, Moon
-} from 'lucide-react';
-import {useTheme} from "@/components/theme-provider"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import Hero from "./Hero"
+import { Button } from "@/components/ui/button";
+import { FileTextIcon, Sun, Moon } from "lucide-react";
+import { useTheme } from "@/lib/useTheme";
+import { motion } from "motion/react";
+import Hero from "./Hero";
 import About from "@/About.tsx";
 import Experience from "@/Experience.tsx";
 import Skills from "@/Skills.tsx";
-import Projects from "@/Projects.tsx";
+import { useMemo } from "react";
 
 export default function App() {
-  const {setTheme} = useTheme()
-  return (<main
-    className="relative min-h-screen  px-4 md:px-12 lg:px-24 py-16 overflow-hidden">
-    <div className="absolute inset-0 z-0">
-      <div
-        className="w-full h-full absolute bg-gradient-to-br from-green-50 via-white to-white dark:from-gray-900 dark:via-gray-950 dark:to-gray-900"></div>
-      <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"
-           className="w-full h-full absolute object-cover opacity-10">
-        <defs>
-          <pattern id="dots" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-            <circle cx="1" cy="1" r="1" fill="currentColor"/>
-          </pattern>
-        </defs>
-        <rect width="100%" height="100%" fill="url(#dots)"/>
-      </svg>
-    </div>
-
-    <div className="relative z-10">
-      <div className="fixed top-6 right-6 z-50 flex gap-4">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" size="icon">
-              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
-              <Moon
-                className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
-              <span className="sr-only">Toggle theme</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem onClick={() => setTheme("light")}>
-              Light
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("dark")}>
-              Dark
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("system")}>
-              System
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-        <Button variant="outline" asChild>
-          <a href="/Jeet_M_resume.pdf" download>
-            <FileTextIcon className="w-4 h-4 mr-2"/> Resume
-          </a>
-        </Button>
+  const { setTheme, theme } = useTheme();
+  const currentTheme = useMemo(() => {
+    if (theme === "light" || theme === "dark") {
+      return theme;
+    } else {
+      return window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light";
+    }
+  }, [theme]);
+  const toggleTheme = () => {
+    if (currentTheme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+  console.log(currentTheme);
+  return (
+    <main className="relative min-h-screen  px-4 md:px-12 lg:px-24 py-4 overflow-hidden">
+      <div className="absolute inset-0 z-0">
+        <div className="w-full h-full absolute bg-gradient-to-br from-green-50 via-white to-white dark:from-gray-900 dark:via-gray-950 dark:to-gray-900"></div>
+        <svg
+          width="100%"
+          height="100%"
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-full h-full absolute object-cover opacity-10"
+        >
+          <defs>
+            <pattern
+              id="dots"
+              x="0"
+              y="0"
+              width="20"
+              height="20"
+              patternUnits="userSpaceOnUse"
+            >
+              <circle cx="1" cy="1" r="1" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width="100%" height="100%" fill="url(#dots)" />
+        </svg>
       </div>
 
-      {/* Hero Section */}
-      <Hero/>
+      <div className="relative">
+        <div className="flex gap-4 justify-end">
+          <motion.div layout transition={{ duration: 1 }}>
+            <Button variant={"ghost"} onClick={toggleTheme}>
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            </Button>
+          </motion.div>
+          {/* <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu> */}
+          <Button variant="outline" asChild>
+            <a href="/Jeet_M_resume.pdf" download>
+              <FileTextIcon className="w-4 h-4 mr-2" /> Resume
+            </a>
+          </Button>
+        </div>
 
-      {/* About Section */}
-      <About/>
+        {/* Hero Section */}
+        <Hero />
 
-      {/* Experience Section */}
-      <Experience/>
+        {/* About Section */}
+        <About />
 
-      {/* Skills Section */}
-      <Skills/>
+        {/* Experience Section */}
+        <Experience />
 
-      {/* Projects Section */}
-      <Projects/>
-    </div>
-  </main>);
+        {/* Skills Section */}
+        <Skills />
+      </div>
+    </main>
+  );
 }
--- src/Skills.tsx
+++ src/Skills.tsx
@@ -1,40 +1,53 @@
-import {motion} from "motion/react";
-import {CodeIcon, DatabaseIcon, GitBranchIcon, Braces} from "lucide-react";
+import { motion } from "motion/react";
+import {
+  CodeIcon,
+  DatabaseIcon,
+  GitBranchIcon,
+  Braces,
+  Workflow,
+  Container,
+} from "lucide-react";
 
 const skills = [
-  {name: 'React.js', icon: <Braces/>},
-  {name: 'Next.js', icon: <Braces/>},
-  {name: 'Redux', icon: <Braces/>},
-  {name: 'Node.js', icon: <Braces/>},
-  {name: 'HTML', icon: <CodeIcon/>},
-  {name: 'CSS', icon: <CodeIcon/>},
-  {name: 'Tailwindcss', icon: <CodeIcon/>},
-  {name: 'TypeScript', icon: <CodeIcon/>},
-  {name: 'PHP', icon: <CodeIcon/>},
-  {name: 'MySQL', icon: <DatabaseIcon/>},
-  {name: 'Git', icon: <GitBranchIcon/>}
+  { name: "React.js", icon: <Braces /> },
+  { name: "Next.js", icon: <Braces /> },
+  { name: "Redux", icon: <Braces /> },
+  { name: "Node.js", icon: <Braces /> },
+  { name: "TypeScript", icon: <Braces /> },
+  { name: "HTML", icon: <CodeIcon /> },
+  { name: "CSS", icon: <CodeIcon /> },
+  { name: "Tailwindcss", icon: <CodeIcon /> },
+  { name: "Styled Component", icon: <CodeIcon /> },
+  { name: "PostgreSQL", icon: <DatabaseIcon /> },
+  { name: "Git", icon: <GitBranchIcon /> },
+  { name: "Github Actions", icon: <Workflow /> },
+  { name: "Docker", icon: <Container /> },
 ];
 const Skills = () => {
-  return (<section className="mb-24" id="skills">
-    <motion.div
-      initial={{opacity: 0}}
-      whileInView={{opacity: 1}}
-      transition={{duration: 0.6}}
-    >
-      <h3 className="text-2xl font-medium mb-8">Skills</h3>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
-        {skills.map((skill, index) => (<motion.div
-          key={index}
-          className="flex items-center space-x-3 p-4 bg-white/70 backdrop-blur-md dark:bg-white/10 rounded-xl shadow-sm"
-          whileHover={{scale: 1.05}}
-          transition={{type: 'spring', stiffness: 300}}
-        >
-          <div className="text-green-600">{skill.icon}</div>
-          <div className="text-lg font-medium">{skill.name}</div>
-        </motion.div>))}
-      </div>
-    </motion.div>
-  </section>)
-}
+  return (
+    <section className="mb-24" id="skills">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.6 }}
+      >
+        <h3 className="text-2xl font-medium mb-8">Skills</h3>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+          {skills.map((skill, index) => (
+            <motion.div
+              key={index}
+              className="flex items-center space-x-3 p-4 bg-white/70 backdrop-blur-md dark:bg-white/10 rounded-xl shadow-sm"
+              whileHover={{ scale: 1.05 }}
+              transition={{ type: "spring", stiffness: 300 }}
+            >
+              <div className="text-green-600">{skill.icon}</div>
+              <div className="text-lg font-medium">{skill.name}</div>
+            </motion.div>
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 
-export default Skills
+export default Skills;
